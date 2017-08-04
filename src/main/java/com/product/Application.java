3c2845bd14b7f2fb6b34080468c7174b4a5bcdee
ketulvinayshah <ketulvinayshah@gmail.com>
2017-08-04 00:41:57 -0700

    removed hystrix dashboard

--- src/main/java/com/product/Application.java
+++ src/main/java/com/product/Application.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
@@ -15,7 +14,6 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableCircuitBreaker
-@EnableHystrixDashboard
 @SuppressWarnings("unused")
 public class Application {
     public static void main(String[] args){
